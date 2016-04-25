f34c3419e5f9235b55bfe20585049d8b2d451900
Krieg Benally <kriegbenally@gmail.com>
2016-04-25 15:55:09 -0600

    this better work

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <h1>This is Aarick's project</h1>
-  <h3>this isvstupid
-  </h3>
+  <h3>this isvstupid</h3>
+  <h1>Hello, I don't like this shhhhhhh </h1>
   </body>
 </html>
